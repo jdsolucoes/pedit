c0c957806fe79bbe166e874b6367d15f37d64ebe
Eduardo de Oliveira Padoan <eduardo.padoan@skyone.solutions>
2015-11-09 14:29:17 -0200

    Fix line editing and copy IPython's %cpaste behaviourLines starting

--- pedit.py
+++ pedit.py
@@ -6,6 +6,11 @@ import readline
 
 import os
 
+try:
+    get_input = raw_input
+except NameError:
+    # Py3k
+    get_input = input
 
 try:
     readline.read_init_file(os.path.join(os.path.expanduser('~'), 'pedit.rc'))
@@ -21,18 +26,26 @@ if __name__ == '__main__':
         print("git config --global core.editor {}".format(sys.argv[0]))
         sys.exit(1)
 
-    print('Enter commit message bellow. Terminate with Ctrl + D.')
+    print("Enter commit message bellow; enter '--' alone on the line to stop")
     with open(filename, 'r+') as file_obj:
         first_line = file_obj.readlines()[0].rstrip()
         file_obj.seek(0)
         if first_line and 'Merge' in first_line:
             print('Press Ctrl + D to keep message: "{}"'.format(first_line))
-        try:
-            lines = sys.stdin.readlines()
-            message = ''.join(lines)
-            if not message and first_line:
-                message = first_line
-        except KeyboardInterrupt:
-            print("Canceled!")
-            sys.exit(1)
+        lines = []
+        while True:
+            try:
+                line = get_input(':')
+            except EOFError:
+                print('<eof>')
+                break
+            except KeyboardInterrupt:
+                print("Canceled!")
+                sys.exit(1)
+            if line == '--':
+                break
+            lines.append(line)
+        message = '\n'.join(lines)
+        if not message and first_line:
+            message = first_line
         file_obj.write(message)
